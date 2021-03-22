51aa6a24ba7c69f013f4c29232e77903ead11198
sam <samjessep77@gmail.com>
2021-03-22 15:57:48 +1300

    removed placeholder styles

--- resources/views/web/landing/index.blade.php
+++ resources/views/web/landing/index.blade.php
@@ -5,8 +5,7 @@
 @endsection
 
 @section('content')
-  {{-- <h2 class="text-center text-2xl">Under Construction</h2> --}}
-  <div class="flex md:w-4/5 w-full bg-red-500 mx-auto mt-5 p-3 flex-wrap justify-around">
+  <div class="flex md:w-4/5 w-full bg-white rounded mx-auto mt-5 p-3 flex-wrap justify-around">
     @foreach ($games as $game)
       <x-game.card :game="$game" :user="Auth::user()"/>
     @endforeach
--- resources/views/web/layouts/error.blade.php
+++ resources/views/web/layouts/error.blade.php
@@ -1,7 +1,8 @@
 @extends('web.layouts.app')
 
 @section('content')
-<div class="p-auto bg-red-500">
+<div class="p-20">
 @yield('error-body')
+<p class="text-lg text-center mt-5">Click <a href="{{route('home')}}" class="link">Here</a> to go home</p>
 </div>
 @endsection
